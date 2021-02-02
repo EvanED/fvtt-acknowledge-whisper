26fa01b4136d80498133e4af5b58d48333712906
Evan Driscoll <evaned@gmail.com>
2021-02-01 22:07:58 -0600

    Communicate via flags, and show the sender whether it is ack'ed

--- dist/acknowledge-whisper.js
+++ dist/acknowledge-whisper.js
@@ -2,20 +2,12 @@ const MODULE_NAME = "acknowledge-whisper";
 const ACK_FLAG_NAME = "needs-acknowledgement";
 
 function acknowledge_whisper(id) {
-    const chat_li = document.querySelector(`li[data-message-id="${id}"]`);
-    chat_li.style = "";
-
-    const button = chat_li.querySelector("button");
-    chat_li.removeChild(button);
-
+    // This function used to remove the button -- but 'setFlag'
+    // results in an updateChatMessage which results in a re-render
+    // anyway, and the html is apparently recreated from the
+    // template. The normal checking suffices then.
     const message = game.messages.get(id);
-    console.log(message);
-    const sender_id = message.data.user;
-    ChatMessage.create({
-	type: CHAT_MESSAGE_TYPES.WHISPER,
-	whisper: [sender_id],
-	content: `[Acknowledging ${message.data.content}]`,
-    });
+    message.setFlag(MODULE_NAME, ACK_FLAG_NAME, null);
 }
 
 Hooks.on("createChatMessage", function(chat_message) {
@@ -28,15 +20,20 @@ Hooks.on("createChatMessage", function(chat_message) {
 })
 
 Hooks.on("renderChatMessage", function(chat_message, html, data) {
-    const flag = chat_message.getFlag(MODULE_NAME, ACK_FLAG_NAME);
+    const needs_ack = chat_message.getFlag(MODULE_NAME, ACK_FLAG_NAME);
     if (chat_message.data.type == CHAT_MESSAGE_TYPES.WHISPER
-	&& data.author.data._id != game.user.data._id
+	&& needs_ack
     ) {
-	html.append("<button>acknowledge</button>");
+	if (data.author.data._id != game.user.data._id) {
+	    html.append("<button>acknowledge</button>");
 
-	const button = html.find("button");
-	button.click(() => acknowledge_whisper(data.message._id));
+	    const button = html.find("button");
+	    button.click(() => acknowledge_whisper(data.message._id));
 
-	html.css("background-color", "red");
+	    html.css("background-color", "red");
+	}
+	else {
+	    html.css("background-color", "blue");
+	}
     }
 });
